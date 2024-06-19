b52cf2809e3dede264f1e6dfb7960ad95fe18135
ZZ3n <ndsrhkd@naver.com>
2024-06-19 15:56:05 +0900

    init

---
+++ src/main/java/org/school/kakao/githubactiontest/GithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package org.school.kakao.githubactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/school/kakao/githubactiontest/GithubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.school.kakao.githubactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
